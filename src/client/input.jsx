4a987ad42f8193e7dad44de9f5519cadf17f5117
vwang4536 <vwang4536@gmail.com>
2019-03-01 17:46:22 -0800

    added propvalidation for input component for best practice

--- src/client/input.jsx
+++ src/client/input.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 const Input = (props) => {
   const { handleCost, handleNumberOfDays, handleStartDate } = props;
@@ -11,4 +12,9 @@ const Input = (props) => {
   );
 };
 
+Input.propTypes = {
+  handleCost: PropTypes.func,
+  handleStartDate: PropTypes.func,
+  handleNumberOfDays: PropTypes.func,
+};
 export default Input;
