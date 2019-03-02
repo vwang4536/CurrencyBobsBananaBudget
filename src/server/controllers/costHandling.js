629ad109338f6622cd638094b81c0e007a71dedc
vwang4536 <vwang4536@gmail.com>
2019-03-01 18:16:58 -0800

    created errorhandling for controller

--- src/server/controllers/costHandling.js
+++ src/server/controllers/costHandling.js
@@ -6,3 +6,16 @@ const client = redis.createClient(REDIS_PORT);
 client.on('connect', () => {
   console.log('Connected to Redis...');
 });
+
+const costHandling = (req, res, next) => {
+  const { startDate, numberOfDays } = req.body;
+  if (startDate === undefined || numberOfDays === undefined) {
+    return res.status(400).send('ERROR! Start date or number of days need to be set');
+  }
+  if (numberOfDays === 0) {
+    return res.send({ totalCost: '0.00' });
+  }
+  if (numberOfDays < 0) {
+    return res.status(400).send('ERROR! Number of days needs to be 0 or greater');
+  }
+}
